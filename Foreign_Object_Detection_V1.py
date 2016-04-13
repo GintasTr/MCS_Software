f343021d4cd43279e4581255631aaa6770499af4
Gintas Trumpa <Gintas.Tr@gmail.com>
2016-04-13 08:08:46 +0300

    LED_sequence is good. Handle position is good (a bit sensitive to hands, requires 45 degrees at slope) Coolant handle position seems to be good - needs validation in different environments Foreign object presence seems to be good - needs validation in different environments. Multiple objects can be implemented.

--- Foreign_Object_Detection_V1.py
+++ Foreign_Object_Detection_V1.py
@@ -18,85 +18,85 @@ def GetImage():
 
 
 # Function to detect valve Handle:
-def HandleDetection(img, coords, data):
+def ObjectDetection(img, coords, data, object_area):
     Std_constant = 5                                            # Describes how many std dev of value to include
     min_value = 30                                              # Minimal illumination threshold
                                                                 # Derive minimum saturation. As a reminder: hsv_data =
                                                                 # {"avg_hue": meanHue, "avg_sat": meanSat, "std_sat": stdSat}
+    min_area = object_area/4                                    # Derive minimum and maximum area objects can take
+    max_area = object_area*4
     minsaturation = (data["avg_sat"]- Std_constant * data["std_sat"])
     img = img.toHSV()                                           # Convert image to HSV colour space
     blobs_threshold = 100                                       # Specify blobs colour distance threshold
-    blobs_min_size =  1000                                       # Specify minimum blobs size
                                                                 # Apply filters to the image TODO: calibrate or change the filtering
     filtered = img.hueDistance(color = data["avg_hue"],
                                minsaturation = minsaturation,
                                minvalue = min_value)
     filtered = filtered.invert()                                # Invert black and white (to have LED as white)
     filtered = filtered.morphClose()                             # Perform morphOps TODO: look for better options
-    all_blobs = filtered.findBlobs(threshval = blobs_threshold, minsize=blobs_min_size)
+    all_blobs = filtered.findBlobs(threshval = blobs_threshold, minsize= min_area, maxsize = max_area )
     if all_blobs > 1:                                           # If more than 1 blob found
         all_blobs.sortDistance(point =(coords[0], coords[1]))   # Sort based on distance from mouse click
     elif all_blobs < 1:
         return "No blobs found"
-    m_Handle = all_blobs[-1]                                       # m_Handle is the closes blob to the click
-    return m_Handle
+    foreign_object = all_blobs[-1]                                       # foreign_object is the closes blob to the click
+    return foreign_object
 
 
 # READ THE DATA STORED IN CALIBRATION FILE
 def read_calibration_data():
-    storage = open("valve_handle_data.txt")
-    Handle_coord_x = float(storage.readline().split()[1])
-    Handle_coord_y = float(storage.readline().split()[1])
-    closed_angle_stored = float(storage.readline().split()[1])
-    open_angle_stored = float(storage.readline().split()[1])
-    closed_average_hue = float(storage.readline().split()[1])
-    closed_average_sat = float(storage.readline().split()[1])
-    closed_std_sat = float(storage.readline().split()[1])
+    storage = open("foreign_object_data.txt")
+    object_coord_x = float(storage.readline().split()[1])
+    object_coord_y = float(storage.readline().split()[1])
+    object_name_stored = (storage.readline().split()[1:])
+    object_area_stored = float(storage.readline().split()[1])
+    object_rect_distance_stored = float(storage.readline().split()[1])
+    object_aspect_ratio_stored = float(storage.readline().split()[1])
+    object_average_hue = float(storage.readline().split()[1])
+    object_average_sat = float(storage.readline().split()[1])
+    object_std_sat = float(storage.readline().split()[1])
     storage.close()
 
-    calibration_data = {"Handle_coord_x": Handle_coord_x,
-                        "Handle_coord_y": Handle_coord_y,
-                        "closed_angle_stored": closed_angle_stored,
-                        "open_angle_stored": open_angle_stored,
-                        "closed_average_hue": closed_average_hue,
-                        "closed_average_sat": closed_average_sat,
-                        "closed_std_sat": closed_std_sat
+    calibration_data = {"object_coord_x": object_coord_x,
+                        "object_coord_y": object_coord_y,
+                        "object_name_stored": object_name_stored,
+                        "object_area_stored": object_area_stored,
+                        "object_rect_distance_stored": object_rect_distance_stored,
+                        "object_aspect_ratio_stored": object_aspect_ratio_stored,
+                        "object_average_hue": object_average_hue,
+                        "object_average_sat": object_average_sat,
+                        "object_std_sat": object_std_sat
                         }
+    print calibration_data
     return calibration_data
 
 
 # Function to scan the image
-def scanning_procedure(Handle_coords, colour_data):
-    img = GetImage()                                           # Get the image
-    Handle = HandleDetection(img, Handle_coords, colour_data)  # Try to detect the handle
-    return Handle                                              # Return the result
+def scanning_procedure(object_coords, colour_data, object_area):
+    img = GetImage()                                                   # Get the image
+    foreign_object = ObjectDetection(img, object_coords, colour_data, object_area)
+                                                                       # Try to detect the object
+    return foreign_object                                              # Return the result
 
 
 # Function to process the results from images
-def process_a_result(handle, closed_angle, open_angle):
-    middle_angle = abs(closed_angle-open_angle)/2           # Find the middle distance between two angles
-    if handle == "No blobs found":                          # If no blobs were found
+def process_a_result(foreign_object):
+    if foreign_object == "No blobs found":                          # If no blobs were found
         return "No blobs found"
-    if abs(handle.angle() - closed_angle) > middle_angle:   # Check if handle is open or closed
-        return "Open"
-    else:
-        return "Closed"
+    return "Object is present"                                      # If specific blob was found
 
 
 # Function to calculate average of the results and return the final one
 def calculate_average(results):
     Possibilities = {                 # Get the dictionary with all the options
     "Not_found": 0,                    # Initialise variable for "No blobs found"
-    "Open": 0,                        # Initialise variable for "Open"
-    "Closed" : 0                      # Initialise variable for "Closed"
+    "Object is present": 0,                        # Initialise variable for "Open"
     }
     for variable in results:                    # Increment each time the specific result is acquired
         if variable[0] == "N":
             Possibilities["Not_found"] += 1
         elif variable[0] == "O":
-            Possibilities["Open"] += 1
-        elif variable[0] == "C":
-            Possibilities["Closed"] += 1
+            Possibilities["Object is present"] += 1
         else:
             print "Something strange happened." # If none of the results fit (Not possible)
     Final_result = max(Possibilities, key=Possibilities.get)
@@ -120,23 +120,28 @@ if not exists("valve_handle_data.txt"):
 calibration_data = read_calibration_data()
 
 # RENAMING FOR EASIER ACCESS:
-Handle_coord_x = calibration_data['Handle_coord_x']
-Handle_coord_y = calibration_data['Handle_coord_y']
-closed_angle_stored = calibration_data['closed_angle_stored']
-open_angle_stored = calibration_data['open_angle_stored']
-closed_average_hue = calibration_data['closed_average_hue']
-closed_average_sat = calibration_data['closed_average_sat']
-closed_std_sat = calibration_data['closed_std_sat']
+object_coord_x = calibration_data['object_coord_x']
+object_coord_y = calibration_data['object_coord_y']
+object_name = calibration_data['object_name_stored']
+object_area = calibration_data['object_area_stored']
+object_rect_distance = calibration_data['object_rect_distance_stored']
+object_aspect_ratio = calibration_data['object_aspect_ratio_stored']
+object_average_hue = calibration_data['object_average_hue']
+object_average_sat = calibration_data['object_average_sat']
+object_std_sat = calibration_data['object_std_sat']
 
-Handle_coords = (Handle_coord_x, Handle_coord_y)
-colour_data = {"avg_hue": closed_average_hue,
-               "avg_sat": closed_average_sat,
-               "std_sat": closed_std_sat}
+object_coords = (object_coord_x, object_coord_y)
+colour_data = {"avg_hue": object_average_hue,
+               "avg_sat": object_average_sat,
+               "std_sat": object_std_sat}
 
 
 for i in range(0, len(results)):
-    result = scanning_procedure(Handle_coords,colour_data)
-    processed_result = process_a_result(result, closed_angle_stored, open_angle_stored)
+
+    # Try to detect foreign object TODO: Implement aspect ratio and sqare distance measurements, modify the filtering values
+    foreign_object_present = scanning_procedure(object_coords,colour_data, object_area)
+
+    processed_result = process_a_result(foreign_object_present)
     results[i] = processed_result
     print results[i]
 
